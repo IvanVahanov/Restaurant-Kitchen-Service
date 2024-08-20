d3208c647d18fdbd41618fb89d6fa10a2e2f066e
Ivan Vahanov <ivan.seeker6@gmail.com>
2024-08-20 08:13:35 +0300

    Add views

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Cook, Dish, DishType, Ingredient
+from .forms import DishForm, DishTypeForm, IngredientForm
 
-# Create your views here.
+
+def dish_list(request):
+    dishes = Dish.objects.all()
+    return render(request, 'dish_list.html', {'dishes': dishes})
+
+
+def cook_list(request):
+    cooks = Cook.objects.all()
+    return render(request, 'cook_list.html', {'cooks': cooks})
+
+
+def dish_detail(request, pk):
+    dish = get_object_or_404(Dish, pk=pk)
+    return render(request, 'dish_detail.html', {'dish': dish})
+
+
+def add_dish(request):
+    if request.method == "POST":
+        form = DishForm(request.POST)
+        if form.is_valid():
+            dish = form.save()
+            return redirect('dish_detail', pk=dish.pk)
+    else:
+        form = DishForm()
+    return render(request, 'add_dish.html', {'form': form})
